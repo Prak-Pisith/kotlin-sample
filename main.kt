038657c8487fcce7f44fcb1dd87693f1db007edb
pisithprak <pisith.p-external@webtiga.com>
2024-10-27 19:10:16 +0700

    Implemented Data-Classes and Coroutines

--- main.kt
+++ main.kt
@@ -13,4 +13,31 @@ fun main(args: Array<String>) {
     var length = nullableString?.length ?: 0
     println(length)
 
+    // Extension Functions
+    fun String.removeFirstLastChar(): String = this.substring(1, this.length - 1)
+    fun String.addExclamation(): String = this + "!"
+
+    var normalString = "Hello, World"
+    println(normalString.addExclamation())
+    println(normalString.removeFirstLastChar())
+
+
+    // Data Classes
+    val person = Persona("John", 25)
+    println(person)
+    val person2 = person.copy(name = "Jane")
+    println(person2)
+
+}
+
+// Data Classes
+data class Persona(val name: String, val age: Int)
+
+// Coroutines for Asynchronous Programming
+suspend fun fetchUserData() {
+    coroutineScope {
+        val user = async { fetchUser() }
+        val profile = async { fetchProfile() }
+        processData(user.await(), profile.await())
+    }
 }
